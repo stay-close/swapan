ef03d039f69a41340fe34e63376676d9ea31e8d0
stay-close <swapansapu2@gmail.com>
2024-03-15 22:36:25 +0600

    main menu position fixed

--- js/slider.js
+++ js/slider.js
@@ -13,13 +13,13 @@ $('.skills_section').owlCarousel({
     nav:true,
     responsive:{
         0:{
-            items:2
+            items:1
         },
         600:{
-            items:2
+            items:1
         },
         1000:{
-            items:4
+            items:3
         }
     }
 })
