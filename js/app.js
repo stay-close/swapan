9e2859937178f4dcc3d69ebb69c7ec79934e4f4c
stay-close <swapansapu2@gmail.com>
2024-03-16 02:03:50 +0600

    toltip problem solved

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Code for BootStrap Tooltip
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
