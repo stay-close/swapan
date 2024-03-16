e24e42748934a62c674670ef913e3d9fedfc2bb6
stay-close <swapansapu2@gmail.com>
2024-03-16 14:57:28 +0600

    some image and resuse area design done!

--- js/slider.js
+++ js/slider.js
@@ -49,3 +49,28 @@ $('.profile_slider').owlCarousel({
         }
     }
 })
+// Main Skilss area slider
+$('.main_skills').owlCarousel({
+    rtl:true,
+    center:true,
+    loop:true,
+    slideTransition: 'linear',
+    autoplay:true,
+    autoplayTimeout: 6000,
+    autoplaySpeed: 6000,
+    autoplayHoverPause: true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
