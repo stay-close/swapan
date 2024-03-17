23e2c08513110641b826c202050290917e702957
stay-close <swapansapu2@gmail.com>
2024-03-17 23:05:34 +0600

    Profile section fixed for mobile devices and slider item fixed for mobile devices

--- js/slider.js
+++ js/slider.js
@@ -39,10 +39,10 @@ $('.profile_slider').owlCarousel({
     dots:false,
     responsive:{
         0:{
-            items:2
+            items:3
         },
         600:{
-            items:2
+            items:3
         },
         1000:{
             items:3
@@ -64,13 +64,13 @@ $('.main_skills').owlCarousel({
     dots:false,
     responsive:{
         0:{
-            items:2
+            items:1
         },
         600:{
-            items:2
+            items:1
         },
         1000:{
-            items:4
+            items:3
         }
     }
 })
